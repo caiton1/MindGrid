a2d22c7b31197842abc5f45f4973e70c25d8276d
caiton1 <connor.nicolai.aiton@gmail.com>
2025-03-26 13:31:32 -0700

    removing sass

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
-import './css/custom.css';
-import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 import { ThemeProvider } from './stores/ThemeContext';
 import { EditModeProvider } from './stores/EditModeConstext';
 
